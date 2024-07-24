2a0710fc4dcd7e5b7db6e0674916cbe74422cd05
UnFriend-PL <104201558+UnFriend-PL@users.noreply.github.com>
2024-07-24 18:31:24 +0200

    konfiguracja bazy danych z azure

---
+++ fizjobackend/DbContexts/FizjoDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace fizjobackend.DbContexts
+{
+    public class FizjoDbContext : DbContext
+    {
+        public FizjoDbContext(DbContextOptions<FizjoDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- fizjobackend/Program.cs
+++ fizjobackend/Program.cs
@@ -1,4 +1,10 @@
 
+using fizjobackend.DbContexts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 namespace fizjobackend
 {
     public class Program
@@ -13,8 +19,12 @@ namespace fizjobackend
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddDbContext<FizjoDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
             var app = builder.Build();
+            TestDatabaseConnection(app);
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -24,13 +34,29 @@ namespace fizjobackend
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
+        }
+        private static void TestDatabaseConnection(WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                var context = services.GetRequiredService<FizjoDbContext>();
+
+                try
+                {
+                    context.Database.OpenConnection();
+                    context.Database.CloseConnection();
+                    logger.LogInformation("Database connection test succeeded.");
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Database connection test failed.");
+                }
+            }
         }
     }
 }
