c830a457c739159be5e4297cc8b328b0f974a35f
dominikakorol02 <dominikakorol02@gmail.com>
2024-08-15 14:14:13 +0200

    Base structure of medical survey.

---
+++ fizjoclient/app/medical-survey/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+import { useState } from "react";
+import styles from "./medicalsurvey.module.scss";
+
+const MedicalSurveyPage = () => {
+
+
+  return (
+    <div className="page">
+      Domi
+    </div>
+  );
+};
+
+export default MedicalSurveyPage 
