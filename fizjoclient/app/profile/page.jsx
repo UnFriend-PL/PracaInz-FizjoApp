f57038a5ed3c5517c182dfe93ce01bf939073317
DjDecay21 <mrychert12@gmail.com>
2024-09-03 18:47:02 +0200

    Edit Form

--- fizjoclient/app/profile/page.jsx
+++ fizjoclient/app/profile/page.jsx
@@ -1,19 +1,30 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import Link from "next/link";
 import styles from "./profile.module.scss";
 import apiService from "../services/apiService/apiService";
+import { validateField } from "../validation";
 import { AuthContext } from "../contexts/Auth/authContext";
 
 const Profile = () => {
   const [userProfile, setUserProfile] = useState(null);
+  const [errors, setErrors] = useState({});
   const [loading, setLoading] = useState(true);
+  const [isEditing, setIsEditing] = useState(false);
   const { isAuthenticated } = React.useContext(AuthContext);
+
+  const formatDate = (dateString) => {
+    return dateString ? dateString.split("T")[0] : "";
+  };
+
   const getUserInfo = async (e) => {
     e.preventDefault();
     try {
       const response = await apiService.get("/User/GetInfo", null, true);
       if (response.success) {
-        return response.data;
+        const userData = response.data;
+        userData.dateOfBirth = formatDate(userData.dateOfBirth);
+        return userData;
       } else {
         throw new Error(response.data);
       }
@@ -23,7 +34,66 @@ const Profile = () => {
       setLoading(false);
     }
   };
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
 
+  const handleSave = async () => {
+    if (!isAuthenticated) {
+      console.error("User is not authenticated");
+      return;
+    }
+    const newErrors = {};
+    Object.keys(userProfile).forEach((key) => {
+      const error = validateField(key, userProfile[key]);
+      if (error) {
+        newErrors[key] = error;
+      }
+    });
+    setErrors(newErrors);
+    if (Object.keys(newErrors).length === 0) {
+      try {
+        const formattedProfile = {
+          firstName: userProfile.firstName || "",
+          lastName: userProfile.lastName || "",
+          gender: userProfile.gender || "",
+          country: userProfile.country || "",
+          city: userProfile.city || "",
+          streetWithHouseNumber: userProfile.streetWithHouseNumber || "",
+          postCode: userProfile.postCode || "",
+          pesel: userProfile.pesel || "",
+          dateOfBirth: userProfile.dateOfBirth || "",
+          email: userProfile.email || "",
+          phoneNumber: userProfile.phoneNumber || "",
+        };
+
+        const response = await apiService.put(
+          "/User/EditInfoUser",
+          formattedProfile,
+          true
+        );
+        if (response.success) {
+          setIsEditing(false);
+        } else {
+          throw new Error(response.data);
+        }
+      } catch (error) {
+        console.error("Error updating user info:", error);
+      }
+    } else {
+      console.error("Validation errors:", newErrors);
+    }
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserProfile((prevProfile) => ({
+      ...prevProfile,
+      [name]: value,
+    }));
+
+    const error = validateField(name, value);
+    setErrors((prevErrors) => ({ ...prevErrors, [name]: error }));
+  };
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -31,6 +101,7 @@ const Profile = () => {
         setUserProfile(data);
       } catch (error) {
         console.error("Error fetching user info:", error);
+        console.log(userProfile.dateOfBirth);
       }
     };
     if (isAuthenticated) fetchData();
@@ -63,9 +134,35 @@ const Profile = () => {
             <span className={styles.label}>
               {key.replace(/([A-Z])/g, " $1").trim()}:
             </span>
-            <span className={styles.value}>{userProfile[key]}</span>
+            {isEditing ? (
+              <>
+                <input
+                  type="text"
+                  name={key}
+                  value={userProfile[key]}
+                  onChange={handleChange}
+                  className={styles.value}
+                />
+                {errors[key] && (
+                  <div className={styles.error}>{errors[key]}</div>
+                )}
+              </>
+            ) : (
+              <span className={styles.profileCardEdit}>{userProfile[key]}</span>
+            )}
           </div>
         ))}
+        <div className={styles["nav-links"]}>
+          {isEditing ? (
+            <button onClick={handleSave} className={styles["nav-link"]}>
+              Save
+            </button>
+          ) : (
+            <button onClick={handleEdit} className={styles["nav-link"]}>
+              Edit
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
