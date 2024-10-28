79da3ecf212a46f5dc84da26842fa0cb9a0b4460
DjDecay21 <mrychert12@gmail.com>
2024-10-28 20:45:03 +0100

    Modified the profile display, including the edit and save buttons. Users can now edit their data, and basic validation for PESEL has been implemented. Additionally, a button has been added to display a list of options, which currently does nothing.

--- fizjoclient/app/profile/page.jsx
+++ fizjoclient/app/profile/page.jsx
@@ -5,7 +5,7 @@ import apiService from "../services/apiService/apiService";
 import { AuthContext } from "../contexts/auth/authContext";
 import { UserContext } from "../contexts/user/userContext";
 import { LanguageContext } from "../contexts/lang/langContext";
-import { FaRegEdit } from "react-icons/fa";
+import { FaCaretDown } from "react-icons/fa";
 import pl from "./locales/pl.json";
 import en from "./locales/en.json";
 
@@ -19,7 +19,13 @@ const Profile = () => {
   const t = locales[language];
   const [isEditing, setIsEditing] = useState(false);
   const [editableFields, setEditableFields] = useState({});
-  const [isDirty, setIsDirty] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [validationErrors, setValidationErrors] = useState({});
+  const [editedUser, setEditedUser] = useState(user);
+
+  useEffect(() => {
+    setEditedUser(user);
+  }, [user]);
 
   const handleEditToggle = (key) => {
     setEditableFields((prev) => ({
@@ -28,17 +34,53 @@ const Profile = () => {
     }));
   };
 
+  const validateField = (key, value) => {
+    const errors = {};
+
+    if (key === "pesel") {
+      if (value.length !== 11) {
+        errors.pesel = t.peselError;
+      } else {
+        const weights = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3];
+        let sum = 0;
+
+        for (let i = 0; i < 10; i++) {
+          sum += parseInt(value[i], 10) * weights[i];
+        }
+
+        const controlDigit = (10 - (sum % 10)) % 10;
+
+        if (controlDigit !== parseInt(value[10], 10)) {
+          errors.pesel = t.invalidPeselError;
+        } else {
+          errors.pesel = "";
+        }
+      }
+    }
+
+    setValidationErrors((prevErrors) => ({
+      ...prevErrors,
+      ...errors,
+    }));
+  };
+
   const handleInputChange = (key, value) => {
-    updateUser((prevUser) => ({
-      ...prevUser,
+    setEditedUser((prevEditedUser) => ({
+      ...prevEditedUser,
       [key]: value,
     }));
-    setIsDirty(true);
+    validateField(key, value);
+  };
+
+  const formatDateToReadable = (dateString) => {
+    const options = { year: "numeric", month: "numeric", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
   };
 
   const enableEditAllFields = () => {
     setIsEditing(!isEditing);
     if (!isEditing) {
+      setEditedUser(user);
       const newEditableFields = Object.keys(user).reduce((acc, key) => {
         acc[key] = true;
         return acc;
@@ -46,18 +88,24 @@ const Profile = () => {
       setEditableFields(newEditableFields);
     } else {
       setEditableFields({});
-      setIsDirty(false);
+      setValidationErrors({});
     }
+    setShowDropdown(false);
   };
 
   const saveChanges = async () => {
     setIsEditing(false);
     setEditableFields({});
-    setIsDirty(false);
+    setValidationErrors({});
     try {
-      console.log(user);
-      const response = await apiService.post("/User/UpdateInfo", user, true);
+      console.log(editedUser);
+      const response = await apiService.post(
+        "/User/UpdateInfo",
+        editedUser,
+        true
+      );
       if (response.success) {
+        updateUser(editedUser);
         return response.data;
       } else {
         throw new Error(response.data);
@@ -67,6 +115,15 @@ const Profile = () => {
     } finally {
       setLoading(false);
     }
+    setShowDropdown(false);
+  };
+
+  const cancelChanges = () => {
+    setIsEditing(false);
+    setEditableFields({});
+    setValidationErrors({});
+    setEditedUser(user); // Reset editedUser back to the original user data
+    setShowDropdown(false);
   };
 
   const getUserInfo = async (e) => {
@@ -98,6 +155,27 @@ const Profile = () => {
     if (isAuthenticated) fetchData();
   }, [isAuthenticated]);
 
+  const toggleDropdown = () => {
+    setShowDropdown((prev) => !prev);
+  };
+
+  const handleMenuClick = (option) => {
+    switch (option) {
+      case "edit":
+        enableEditAllFields();
+        break;
+      case "appointments":
+        console.log("Navigate to Appointments");
+        break;
+      case "opinion":
+        console.log("Navigate to Opinion");
+        break;
+      default:
+        break;
+    }
+    setShowDropdown(false);
+  };
+
   if (!isAuthenticated) {
     return (
       <div className={styles.container}>
@@ -118,44 +196,85 @@ const Profile = () => {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>{t.profile}</h1>
+      <div className={styles.header}>
+        <h1 className={styles.title}>{t.profile}</h1>
+        <div className={styles.dropdownContainer}>
+          <FaCaretDown
+            className={styles.dropdownIcon}
+            onClick={toggleDropdown}
+          />
+          {showDropdown && (
+            <div className={styles.dropdownMenu}>
+              <div onClick={() => handleMenuClick("appointments")}>
+                {t.appointments}
+              </div>
+              <div onClick={() => handleMenuClick("opinion")}>{t.opinions}</div>
+            </div>
+          )}
+        </div>
+      </div>
 
       <div className={styles.profileCard}>
-        <button onClick={enableEditAllFields} className={styles.editButton}>
-          <FaRegEdit className={styles.editIcon} />{" "}
-          {isEditing ? t.saveChanges : t.editAllFields}
-        </button>
         {Object.keys(user)
-          .filter((key) => key !== "Id")
+          .filter((key) => key !== "id")
           .map((key) => (
-            <div className={styles.field} key={key}>
-              <span className={styles.label}>
-                {t[key] || key.replace(/([A-Z])/g, " $1").trim()}:
-              </span>
-              {editableFields[key] ? (
-                <input
-                  type="text"
-                  value={user[key]}
-                  onChange={(e) => handleInputChange(key, e.target.value)}
-                  onBlur={() => handleEditToggle(key)}
-                  className={styles.editableInput}
-                />
-              ) : (
-                <span
-                  className={styles.value}
-                  onDoubleClick={() => handleEditToggle(key)}
-                >
-                  {user[key]}
+            <div className={styles.fieldContainer} key={key}>
+              <div className={styles.field}>
+                <span className={styles.label}>
+                  {t[key] || key.replace(/([A-Z])/g, " $1").trim()}:
                 </span>
+
+                {editableFields[key] ? (
+                  key === "dateOfBirth" ? (
+                    <input
+                      type="date"
+                      value={
+                        editedUser[key] ? editedUser[key].slice(0, 10) : ""
+                      }
+                      onChange={(e) => handleInputChange(key, e.target.value)}
+                      className={styles.editableInput}
+                    />
+                  ) : (
+                    <input
+                      type="text"
+                      value={editedUser[key]}
+                      onChange={(e) => handleInputChange(key, e.target.value)}
+                      className={styles.editableInput}
+                    />
+                  )
+                ) : (
+                  <span className={`${styles.value} ${styles.nonEditable}`}>
+                    {key === "dateOfBirth"
+                      ? formatDateToReadable(user[key])
+                      : user[key]}
+                  </span>
+                )}
+              </div>
+
+              {/* Komunikat błędu poniżej pola */}
+              {validationErrors[key] && (
+                <div className={styles.error}>{validationErrors[key]}</div>
               )}
             </div>
           ))}
       </div>
-      {isDirty && (
-        <button onClick={saveChanges} className={styles.saveButton}>
-          {t.saveChanges}
-        </button>
-      )}
+
+      <div className={styles.buttonContainer}>
+        {!isEditing ? (
+          <button onClick={enableEditAllFields} className={styles.editButton}>
+            {t.editButton}
+          </button>
+        ) : (
+          <div>
+            <button onClick={cancelChanges} className={styles.cancelButton}>
+              {t.cancelButton}
+            </button>
+            <button onClick={saveChanges} className={styles.saveButton}>
+              {t.saveChanges}
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
