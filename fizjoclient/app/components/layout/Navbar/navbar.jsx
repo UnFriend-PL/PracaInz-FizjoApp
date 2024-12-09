cf02c65b3b7758e0f7db912ee86d232fa1eda0e8
UnFriend-PL <104201558+UnFriend-PL@users.noreply.github.com>
2024-12-09 20:16:16 +0100

    remove css maps

--- fizjoclient/app/components/layout/Navbar/navbar.jsx
+++ fizjoclient/app/components/layout/Navbar/navbar.jsx
@@ -26,14 +26,12 @@ export default function Navbar() {
 
   const t = locales[language];
 
-  // Ref to handle clicks outside the menu
   const menuRef = useRef(null);
 
-  // Close the menu when clicking outside
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (menuRef.current && !menuRef.current.contains(event.target)) {
-        setIsOpen(false); // Close the menu if click is outside
+        setIsOpen(false);
       }
     };
 
@@ -90,67 +88,67 @@ export default function Navbar() {
   };
 
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.container}>
-        <div className={styles.navbarContent}>
-          <div className={styles.logo}>
-            <Link href="/">{t.logo}</Link>
-          </div>
-          <div className={styles.navLinks}>
-            <GenerateLinks links={MENU_LINKS} />
-          </div>
+      <nav className={styles.navbar}>
+        <div className={styles.container}>
+          <div className={styles.navbarContent}>
+            <div className={styles.logo}>
+              <Link href="/">{t.logo}</Link>
+            </div>
+            <div className={styles.navLinks}>
+              <GenerateLinks links={MENU_LINKS} />
+            </div>
 
-          <div className={styles.languages}>
-            <button
-              className={styles.languageButton}
-              onClick={() => changeLanguage("en")}
-            >
-              EN
-            </button>
-            <button
-              className={styles.languageButton}
-              onClick={() => changeLanguage("pl")}
-            >
-              PL
-            </button>
-          </div>
-
-          <div className={styles.menuButton}>
-            <button
-              type="button"
-              aria-controls="mobileMenu"
-              aria-expanded={isOpen}
-              onClick={toggleMenu}
-            >
-              <svg
-                className="block h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
+            <div className={styles.languages}>
+              <button
+                  className={styles.languageButton}
+                  onClick={() => changeLanguage("en")}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
-                />
-              </svg>
-            </button>
+                EN
+              </button>
+              <button
+                  className={styles.languageButton}
+                  onClick={() => changeLanguage("pl")}
+              >
+                PL
+              </button>
+            </div>
+
+            <div className={styles.menuButton}>
+              <button
+                  type="button"
+                  aria-controls="mobileMenu"
+                  aria-expanded={isOpen}
+                  onClick={toggleMenu}
+              >
+                <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                >
+                  <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div
-        className={`${styles.mobileMenu} ${isOpen ? styles.open : ""}`}
-        id="mobileMenu"
-      >
-        <div className={styles.mobileMenuLinks}>
-          <GenerateLinks links={MENU_LINKS} setIsOpen={setIsOpen} />
+        <div
+            className={`${styles.mobileMenu} ${isOpen ? styles.open : ""}`}
+            id="mobileMenu"
+        >
+          <div className={styles.mobileMenuLinks} ref={menuRef}>
+            <GenerateLinks links={MENU_LINKS} setIsOpen={setIsOpen} />
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
   );
 }
 
@@ -161,21 +159,23 @@ function GenerateLinks({ links, setIsOpen }) {
     const handleClick = (e) => {
       if (action) {
         e.stopPropagation();
-        action(); // Wykonaj akcję, np. logout
+        action();
       }
-      setIsOpen(false); // Zamyka menu po kliknięciu w link
+      if (typeof setIsOpen === "function") {
+        setIsOpen(false);
+      }
     };
 
     return (
-      <Link
-        key={key}
-        href={href}
-        className={className}
-        onClick={handleClick} // Używamy onClick zamiast onclick
-        title={title}
-      >
-        {icon != undefined ? icon : name}
-      </Link>
+        <Link
+            key={key}
+            href={href}
+            className={className}
+            onClick={handleClick}
+            title={title}
+        >
+          {icon != undefined ? icon : name}
+        </Link>
     );
   });
 }
